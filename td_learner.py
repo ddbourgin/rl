142d7011d8f42775fb49c78e97fa6aee319401ec
David Bourgin <ddbourgin@gmail.com>
2017-08-13 11:41:11 -0400

    automatically tile-code if we detect a continuous state space

--- td_learner.py
+++ td_learner.py
@@ -1,30 +1,36 @@
-from time import time
 import itertools
+from time import time
+from collections import defaultdict
 
 import gym
 import numpy as np
 
-from utils import check_discrete
+from utils import check_discrete, tile_state_space
+
 
 class TDLearner(object):
     """
-    A temporal difference learning agent
+    Temporal difference learning agent with SARSA (on-policy) and TD(0)
+    Q-learning (off-policy) updates
     """
 
     def __init__(self, env, **kwargs):
         self.__validate_env__(env)
 
-        self.learning_rate = kwargs['learning_rate']
-        self.episode_len = kwargs['max_episode_len']
-        self.off_policy = kwargs['off_policy']
-        self.gamma = kwargs['discount_factor']
-        self.epsilon = kwargs['epsilon']
+        self.learning_rate = learning_rate
+        self.episode_len = max_episode_len
+        self.off_policy = off_policy
+        self.gamma = discount_factor
+        self.epsilon = epsilon
+        self.n_tilings = n_tilings
+        self.grid_dims = grid_dims
 
         # initialize Q function
-        self.Q = np.random.rand(self.n_states, self.n_actions)
+        self.Q = defaultdict(np.random.rand)
 
     def __validate_env__(self, env):
-        is_multi_obs, is_multi_act = check_discrete(env, 'Temporal Difference')
+        is_multi_obs, is_multi_act, is_disc_obs, is_disc_act = \
+            check_discrete(env, 'Temporal Difference', obs=False)
 
         # action space is multidimensional
         if is_multi_act:
@@ -35,25 +41,20 @@ class TDLearner(object):
             n_actions = env.action_space.n
             one_dim_action = range(n_actions)
 
-        # observation space is multidimensional
-        if is_multi_obs:
-            n_obs = [space.n for space in env.observation_space.spaces]
-            one_dim_obs = list(
-                itertools.product(*[range(i) for i in n_obs]))
-        else:
-            n_obs = env.observation_space.n
-            one_dim_obs = range(n_obs)
+        self.n_actions = np.prod(n_actions)
 
         # create action -> scalar dictionaries
         self.action2num = {act: i for i, act in enumerate(one_dim_action)}
         self.num2action = {i: act for act, i in self.action2num.items()}
 
-        # create obs -> scalar dictionaries
-        self.obs2num = {act: i for i, act in enumerate(one_dim_obs)}
-        self.num2obs = {i: act for act, i in self.obs2num.items()}
+        self.obs_encoder = lambda x: x
+        if not is_disc_obs:
+            self.obs_encoder, _ = \
+                tile_state_space(env, n_tilings, grid_size=(4, 4))
 
-        self.n_actions = np.prod(n_actions)
-        self.n_states = np.prod(n_obs)
+        # initialize obs -> scalar dictionaries
+        self.obs2num = {}
+        self.num2obs = {}
 
     def on_policy_update_Q(self, s, a, r, s_, a_):
         """
@@ -62,27 +63,30 @@ class TDLearner(object):
         """
         # compute the expected value of Q(s', a') given that we are in state s'
         E_Q = np.sum(
-            [self.epsilon_soft_policy(s_, aa) * self.Q[s_, aa]
+            [self.epsilon_soft_policy(s_, aa) * self.Q[(s_, aa)]
              for aa in xrange(self.n_actions)]
         )
 
-        self.Q[s, a] += self.learning_rate * \
-            (r + self.gamma * E_Q - self.Q[s, a])
+        self.Q[(s, a)] += self.learning_rate * \
+            (r + self.gamma * E_Q - self.Q[(s, a)])
 
     def off_policy_update_Q(self, s, a, r, s_):
         """
         Update Q using the off-policy TD(0) Q-learning update
             Q(s, a) <- Q(s, a) + lr * [r + gamma * max_a { Q(s', a) } - Q(s, a)]
         """
-        self.Q[s, a] += self.learning_rate * \
-            (r + self.gamma * np.max(self.Q[s_, :]) - self.Q[s, a])
+        Qs_ = [self.Q[(s_, aa)] for aa in xrange(self.n_actions)]
 
-    def epsilon_soft_policy(self, observation, action=None):
+        self.Q[(s, a)] += self.learning_rate * \
+            (r + self.gamma * np.max(Qs_) - self.Q[(s, a)])
+
+    def epsilon_soft_policy(self, s, action=None):
         """
         pi(a|s) = 1 - epsilon + (epsilon / |A(s)|) IFF a == a*
         pi(a|s) = epsilon / |A(s)|                 IFF a != a*
         """
-        a_star = self.Q[observation, :].argmax()
+        #  a_star = self.Q[s, :].argmax()
+        a_star = np.argmax([self.Q[(s, aa)] for aa in xrange(self.n_actions)])
         p_a_star = 1. - self.epsilon + (self.epsilon / self.n_actions)
         p_a = self.epsilon / self.n_actions
 
@@ -96,9 +100,22 @@ class TDLearner(object):
         action = np.random.multinomial(1, action_probs)
         return self.num2action[action.argmax()]
 
+    def __encode_obs__(self, obs):
+        # incrementally build obs2num/num2obs with experience
+        obs = self.obs_encoder(obs)
+        if obs in self.obs2num.keys():
+            num = self.obs2num[obs]
+        else:
+            num = 0
+            if len(self.obs2num) > 0:
+                num = np.max(self.obs2num.values()) + 1
+            self.obs2num[obs] = num
+            self.num2obs[num] = obs
+        return num
+
     def run_episode(self, env, render=False):
         obs = env.reset()
-        s = self.obs2num[obs]
+        s = self.__encode_obs__(obs)
 
         # generate an action using an epsilon-soft policy
         action = self.epsilon_soft_policy(s)
@@ -112,7 +129,7 @@ class TDLearner(object):
 
             # take action
             obs_, reward, done, info = env.step(action)
-            s_ = self.obs2num[obs_]
+            s_ = self.__encode_obs__(obs_)
 
             # record rewards
             reward_history.append(reward)
@@ -130,11 +147,11 @@ class TDLearner(object):
 
             else:
                 # generate a new action using an epsilon-soft policy
-                action_ = self.epsilon_soft_policy(obs_)
-                a_ = self.action2num[action]
+                action_ = self.epsilon_soft_policy(s_)
+                a_ = self.action2num[action_]
 
                 # expected SARSA on-policy update
-                self.on_policy_update_Q(obs, a, reward, obs_, a_)
+                self.on_policy_update_Q(s, a, reward, s_, a_)
 
                 # update observations and actions
                 obs, s, action, a = obs_, s_, action_, a_
@@ -149,17 +166,22 @@ if __name__ == "__main__":
     env = gym.make('Copy-v0')
 
     # initialize run parameters
-    n_epochs = 100000       # number of episodes to train the q network on
+    n_epochs = 50000        # number of episodes to train the q network on
     epsilon = 0.10          # for epsilon-soft policy during training
     max_episode_len = 200   # max number of timesteps per episode/epoch
     discount_factor = 0.95  # temporal discount factor
-    learning_rate = 0.1     # learning rate parameter
-    off_policy = True       # on_policy = expected SARSA update
-                            # off_policy = Q-learning update
+    learning_rate = 0.05    # learning rate parameter
     render = False          # render runs during training
+    n_tilings = 2 ** 7      # number of tilings to use if state space is continuous
+    grid_dims = [8, 8]      # number of squares in the tiling grid if state
+                            # space is continuous
+    off_policy = True # on_policy = expected SARSA update
+                            # off_policy = Q-learning update
 
     mc_params = \
         {'epsilon': epsilon,
+         'grid_dims': grid_dims,
+         'n_tilings': n_tilings,
          'off_policy': off_policy,
          'learning_rate': learning_rate,
          'max_episode_len': max_episode_len,
